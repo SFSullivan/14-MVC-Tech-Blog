24118c35f90f9e9f4690a70cd87240453fcb3f17
SSullivan <spencersullivan1990@gmail.com>
2022-09-04 15:01:12 -0500

    comment routes typed

---
+++ models/routes/CommRoutes.js
@@ -0,0 +1,14 @@
+const router = require('express').Router();
+const Comment = require("Comments");
+
+router.post("/",async (req,res)=>{
+    try{
+        const comment = await Comment.create(req.body);
+        req.session.comment_id = comment.id;
+        res.redirect('/dashboard');
+    }catch(error){
+        res.status(400).json(err);
+    }
+});
+
+module.exports = router;
