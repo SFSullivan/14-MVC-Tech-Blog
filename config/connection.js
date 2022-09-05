161d0f9e59a1b30332b7c368b9fa2509c71fc8e7
SSullivan <spencersullivan1990@gmail.com>
2022-09-05 14:28:00 -0500

    server typed up

---
+++ config/connection.js
@@ -0,0 +1,18 @@
+const { Sequelize } = require('sequelize');
+require('dotenv').config()
+
+const connection = process.env.JAWSDB_URL ?
+    new Sequelize(process.env.JAWSDB_URL) :
+    new Sequelize(
+        process.env.DB_NAME,
+        process.env.DB_USER,
+        process.env.DB_PASSWORD,
+        {
+            host: 'localhost',
+            dialect: 'mysql',
+            port: 3333,
+            logging: false
+        }
+    );
+
+module.exports = connection;
