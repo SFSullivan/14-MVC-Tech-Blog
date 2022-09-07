5c5a9f7d3abfc1d52625d539d0163a4140355d20
SSullivan <spencersullivan1990@gmail.com>
2022-09-07 09:58:20 -0500

    post routes typed up

--- models/User.js
+++ models/User.js
@@ -1,5 +1,5 @@
 const { Model, DataTypes } = require('sequelize')
-const sequelize = require('../config/db_connection')
+const sequelize = require('../config/connection')
 const bcrypt = require('bcrypt')
 const { createHook } = require('async_hooks')
 
