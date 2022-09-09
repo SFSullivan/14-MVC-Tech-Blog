a43419694a954758774f0814dbbe2a48bb164179
SSullivan <spencersullivan1990@gmail.com>
2022-09-09 11:22:26 -0500

    auth routes typed

---
+++ routes/auth_routes.js
@@ -0,0 +1,53 @@
+const router = require('express').Router();
+
+const User = require('../models/User');
+
+router.post('/registration', (req, res) => {
+    User.findOne({
+        where: {
+            email: req.body.email
+        }
+    }).then(user => {
+        if (user) {
+            return res.send('user already exists');
+        }
+        User.create({
+            email: req.body.email,
+            username: req.body.username,
+            password: req.body.password
+        }).then(user => {
+            req.session.save(() => {
+                req.session.user_id = user.user_id
+                res.redirect('/dashboard')
+            })
+        })
+    })
+})
+
+router.post('/logged', (req, res) => {
+    User.findOne({
+        where: {
+            username: req.body.username
+        }
+    }).then(async user => {
+        if (!user) {
+            return res.redirect('/login')
+        }
+        const validPass = await user.validatePassword(req.body.password, user.password)
+
+        if (!validPass) {
+            return res.redirect('/login')
+        }
+
+        req.session.save(() => {
+            req.session.user_id = user.id;
+            res.redirect('/dashboard')
+        })
+    })
+})
+
+router.get('/logout', (req, res) => {
+    if (!req.session.user_id) return res.redirect('/')
+})
+
+module.exports = router;
